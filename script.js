b8cd31016b9da97b05a8f378c4caa0414ef684ee
krather7 <75811296+krather7@users.noreply.github.com>
2021-03-20 16:06:32 -0500

    12 quotes are assigned to a sign without refresh

--- script.js
+++ script.js
@@ -4,6 +4,30 @@ var parentEL = document.querySelector('.parentDiv');
 var parentEL2 = document.querySelector('.parentDiv2');
 var newDiv = document.querySelector('#newDiv')
 var dailyDiv = document.querySelector('#dailyDiv')
+var clickedItem= "";
+//===========================================================
+let quoteArray=[
+  {"sign": "Capricorn","quote": 0},
+  {"sign": "Aquarius","quote": 0},
+  {"sign": "Pisces","quote": 0},
+  {"sign": "Aries","quote": 0},
+  {"sign": "Taurus","quote": 0},
+  {"sign": "Gemini","quote": 0},
+  {"sign": "Cancer","quote": 0},
+  {"sign": "Leo","quote": 0},
+  {"sign": "Virgo","quote": 0},
+  {"sign": "Libra","quote": 0},
+  {"sign": "Scorpio","quote": 0},
+  {"sign": "Saggittarius","quote": 0}
+  ]
+  
+  for (x=0;x<quoteArray.length;x++)
+  {let j = Math.floor(Math.random() * 1643);
+  quoteArray[x].quote=j}
+  console.log(quoteArray)
+  //=============================================================
+
+
 // var dailyEL = document.querySelector()
 parentEL.addEventListener('click', runFn);
 parentEL2.addEventListener('click', runFn);
@@ -14,7 +38,6 @@ function runFn(e) {
     console.log(e.target);
     console.log('you clicked ', clickedItem)
     getDaily(clickedItem)
-    getQuote()
   }
   e.stopPropagation();
 }
@@ -45,27 +68,58 @@ function getDaily(sign) {
     .catch(err => {
       console.error(err);
     });
-}
+//===================
+
+      if (sign=="Capricorn"){
+      j=quoteArray[0].quote
+      }
+      if (sign=="Aquarius"){
+      j=quoteArray[1].quote
+      }
+      if (sign=="Pisces"){
+      j=quoteArray[2].quote
+      }
+      if (sign=="Aries"){
+      j=quoteArray[3].quote
+      }
+      if (sign=="Taurus"){
+        j=quoteArray[4].quote
+      }
+      if (sign=="Gemini"){
+        j=quoteArray[5].quote
+      }
+      if (sign=="Cancer"){
+         j=quoteArray[6].quote
+      }  
+      if (sign=="Leo"){
+         j=quoteArray[7].quote
+      }
+      if (sign=="Virgo"){
+         j=quoteArray[8].quote
+      }
+      if (sign=="Libra"){
+        j=quoteArray[9].quote
+      }
+      if (sign=="Scorpio"){
+       j=quoteArray[10].quote
+      }
+      if (sign=="Saggittarius"){
+        j=quoteArray[11].quote
+      }
 
 
-function getQuote() {
-
+//===================
   fetch("https://type.fit/api/quotes")
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       // console.log(data);
-      var j = Math.floor(Math.random() * 1643);
       if (data[j].author==null){
         newDiv.innerHTML = '"' + data[j].text + '"' + "-Anonymous"
-        console.log(data[j].author)
        }else{
-      newDiv.innerHTML = '"' + data[j].text + '"' + "-" + data[j].author
+      newDiv.innerHTML = '"' + data[j].text + '"' + "-" + data[j].author}
       document.getElementById("newDiv").style.display = "block";
       document.getElementById("bottomTitle2").style.display = "block";
-      console.log(data[j].author)
-       }
     })
-
 }
